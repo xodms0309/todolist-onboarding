9b7cf6a6c0d5e3e21e17bc7687eda196a1acd5f3
Taeeun Kim <taeeunkim@likelion.net>
2021-11-19 18:13:50 +0900

    :sparkles:  feat: add todo

--- components/TodoInput.tsx
+++
@@ -1,8 +0,0 @@
-function TodoInput(){
-    return(
-        <input 
-        placeholder="todo 입력하기"
-        />
-    );
-}
-export default TodoInput;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,49 @@
 import type { NextPage } from 'next'
-import TodoInput from '../components/TodoInput'
+import { useState } from 'react';
+import {v1} from 'uuid';
+interface ITodoItem {
+  id: string
+  todo: string
+}
 const Home: NextPage = () => {
+  const [todo, setTodo]=useState('');
+  const [todolist, setTodoList]=useState<ITodoItem[]>([]);
+  const onHandleInputChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
+    setTodo(e.target.value);
+}
+  const addTodo = () => {
+    const item:ITodoItem={
+      id:v1(),
+      todo:todo,
+    }
+    if (todo) {
+      setTodoList([...todolist, item])
+    }
+    setTodo('');
+  }
   return (
+  <>
     <header>
       <div>Todolist</div>
-      <TodoInput />
-      <button>Add Todo</button>
+      <input 
+        placeholder='todo 입력하기'
+        value={todo}
+        onChange={onHandleInputChange}
+      />
+      <button onClick={()=>addTodo()}>Add Todo</button>
     </header>
-
+    <ul>     
+      {
+        todolist.map((item)=>{
+          return(
+          <li key={item.id}>
+            {item.todo}
+          </li>
+          )
+        })
+      }
+    </ul> 
+  </>
   )
 }
-
 export default Home
